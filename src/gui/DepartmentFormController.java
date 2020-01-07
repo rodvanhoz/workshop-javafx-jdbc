a0f3144b8be947cf9ed8d3694ec78c314d3f3c4f
Rodrigo Vanhoz Ribeiro <rodrigo.vanhoz@gmail.com>
2020-01-07 20:53:40 -0200

    Observer design pattern to update tableview

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -21,6 +24,8 @@ public class DepartmentFormController implements Initializable {
 
 	private Department entity;
 	private DepartmentService service;
+	
+	private List<DataChangeListener> dataChangedListeners = new ArrayList<>();
 	
 	@FXML
 	private TextField txtId;
@@ -47,7 +52,11 @@ public class DepartmentFormController implements Initializable {
 	public void setDepartmentService(DepartmentService service) {
 		this.service = service;
 	}
-	
+
+	public void subscribleDataChangeListener(DataChangeListener listeners) {
+		dataChangedListeners.add(listeners);
+	}
+
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
 		System.out.println("onBtSaveAction");
@@ -63,6 +72,7 @@ public class DepartmentFormController implements Initializable {
 		try {
 			entity = getFormData();
 			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
 		}
 		catch (DbException e) {
@@ -70,6 +80,13 @@ public class DepartmentFormController implements Initializable {
 		}
 	}
 	
+
+	private void notifyDataChangeListeners() {
+		
+		for (DataChangeListener listener : dataChangedListeners) {
+			listener.onDataChanged();
+		}
+	}
 
 	@FXML
 	public void onBtCancelAction(ActionEvent event) {
--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -8,6 +8,7 @@ import java.util.ResourceBundle;
 import com.sun.javafx.scene.traversal.ParentTraversalEngine;
 
 import application.Main;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
 import javafx.collections.FXCollections;
@@ -28,7 +29,7 @@ import javafx.stage.Stage;
 import model.entities.Department;
 import model.services.DepartmentService;
 
-public class DepartmentListController implements Initializable {
+public class DepartmentListController implements Initializable, DataChangeListener {
 
 	private DepartmentService service;
 	
@@ -95,6 +96,7 @@ public class DepartmentListController implements Initializable {
 			DepartmentFormController controller = loader.getController();
 			controller.setDepartment(obj);
 			controller.updateFormData();
+			controller.subscribleDataChangeListener(this);
 			controller.setDepartmentService(new DepartmentService());
 			
 			Stage dialogStage = new Stage();
@@ -108,5 +110,11 @@ public class DepartmentListController implements Initializable {
 		catch (IOException e) {
 			Alerts.showAlert("IOException", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
+	}
+
+	@Override
+	public void onDataChanged() {
+		
+		updateTableView();
 	}
 }
